ae2bb49f49d533b11d80c6fd6f2ca170c2e97b74
VAO <vvryoung@outlook.com>
2025-02-18 22:42:12 +0800

    remove lib and add mac demo

---
+++ LIbMPVSharp.Avalonia.Demo.Mac/UriResolver.cs
@@ -0,0 +1,17 @@
+﻿using LibMPVSharp.Avalonia.Demo.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibMPVSharp.Avalonia.Demo.Mac
+{
+    internal class UriResolver : IUriResolver
+    {
+        public string? GetRealPath(Uri uri)
+        {
+            return uri.LocalPath;
+        }
+    }
+}
