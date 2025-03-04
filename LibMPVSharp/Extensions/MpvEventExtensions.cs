144fc4ed1b0c5c21ca54f8e6737921b1e71c07e1
vvao <vvryoung@outlook.com>
2025-03-04 17:01:05 +0800

    mpv event

---
+++ LibMPVSharp/Extensions/MpvEventExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibMPVSharp.Extensions
+{
+    public unsafe static class MpvEventExtensions
+    {
+        public static T ReadData<T>(this MpvEvent mpvEvent) where T: struct
+        {
+            return Marshal.PtrToStructure<T>((IntPtr)mpvEvent.data);
+        }
+    }
+}
---
+++ LibMPVSharp/Wraps/DisposableObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibMPVSharp.Wraps
+{
+    internal class DisposableObject : IDisposable
+    {
+        private readonly Action _disposeAction;
+        public DisposableObject(Action disposeAction)
+        {
+            _disposeAction = disposeAction;
+        }
+        public void Dispose()
+        {
+            _disposeAction?.Invoke();
+        }
+    }
+}
